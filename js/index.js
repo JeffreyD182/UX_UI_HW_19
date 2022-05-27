0beefe1946f9ab9bc0cf02a7ee5f3918903d30f6
JeffreyD182 <jeffreydeleon@outlook.com>
2022-05-26 21:20:57 -0400

    Back to top

--- js/index.js
+++ js/index.js
@@ -1,3 +1,26 @@
 // JS test
 
 console.log ("The index.js file is working correctly");
+
+// Back to top
+
+$(document).ready(function(){
+ 
+    //Check to see if the window is top if not then display button
+    $(window).scroll(function(){
+     // Show button after 100px
+     var showAfter = 250;
+     if ($(this).scrollTop() > showAfter ) { 
+      $('.back-to-top').fadeIn();
+     } else { 
+      $('.back-to-top').fadeOut();
+     }
+    });
+    
+    //Click event to scroll to top
+    $('.back-to-top').click(function(){
+     $('html, body').animate({scrollTop : 0},800);
+     return false;
+    });
+    
+   });
