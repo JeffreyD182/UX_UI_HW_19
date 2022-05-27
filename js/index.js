7c6e0be7db35a7c475b6c631ea53b709558f03ae
JeffreyD182 <jeffreydeleon@outlook.com>
2022-05-26 22:17:39 -0400

    back to top button update

--- js/index.js
+++ js/index.js
@@ -4,23 +4,17 @@ console.log ("The index.js file is working correctly");
 
 // Back to top
 
-$(document).ready(function(){
- 
-    //Check to see if the window is top if not then display button
-    $(window).scroll(function(){
-     // Show button after 100px
-     var showAfter = 250;
-     if ($(this).scrollTop() > showAfter ) { 
-      $('.back-to-top').fadeIn();
-     } else { 
-      $('.back-to-top').fadeOut();
-     }
-    });
-    
-    //Click event to scroll to top
-    $('.back-to-top').click(function(){
-     $('html, body').animate({scrollTop : 0},800);
-     return false;
-    });
-    
-   });
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
